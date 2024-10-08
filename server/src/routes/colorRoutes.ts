f0a0050d1351422c6f896efc96c03268b3ff866b
afieve <anthony.fieve@gmail.com>
2024-10-08 11:45:29 +0200

    server: removing ColorsService.ts

--- server/src/routes/colorRoutes.ts
+++ server/src/routes/colorRoutes.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import pino from 'pino';
-import { getHTMLColorTable } from '../services/ColorService';
 
 const router = express.Router();
 const logger = pino();
 
 router.get('/colors', async (req, res) => {
     logger.info(`${req.headers.origin}: GET "/colors": Demande la page HTML 'colors'.`);
-    const html = await getHTMLColorTable();
-    res.send(html);
+    res.send('colors are not a feature yet');
     
 });
 
